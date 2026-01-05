1e7e7d6942fc63cf86529471fc1a6b7765472500
Balazs Fentor <fentorb24@gmail.com>
2026-01-05 22:31:37 +0100

    Implemented geolocation-based weather

--- src/app.py
+++ src/app.py
@@ -1,8 +1,18 @@
 import dash_bootstrap_components as dbc
 import dash_daq as daq
-from dash import Dash, html, dcc, callback, page_container
+from dash import Dash, html, dcc, callback, page_container, no_update
 from dash.dependencies import Input, Output, State
+from dotenv import load_dotenv
+import os
+import weatherapi
+from weatherapi.rest import ApiException
 
+load_dotenv()
+
+configuration = weatherapi.Configuration()
+configuration.api_key['key'] = os.getenv('WEATHERAPI_KEY')
+
+api_instance = weatherapi.APIsApi(weatherapi.ApiClient(configuration))
 
 def main():
     app = Dash(__name__, use_pages=False, external_stylesheets=[dbc.themes.MINTY, dbc.icons.BOOTSTRAP])
@@ -30,7 +40,7 @@ def main():
                         html.Span(
                             [
                                 html.H5("°C"), 
-                                daq.ToggleSwitch(id="temp-format", value=False), 
+                                daq.ToggleSwitch(id="temp-format", value=False, persistence=True, persistence_type="local"), 
                                 html.H5("°F")
                             ], className="switch"
                         )
@@ -40,7 +50,7 @@ def main():
             dbc.CardBody(
                 [
                     html.H1("0°", id="temperature", className="card-title"),
-                    html.H5("Lahti, Finland", id="location")
+                    html.H5("..., ...", id="location")
                 ]
             ),
         ],style={"width": "20rem"})
@@ -49,7 +59,7 @@ def main():
             [
                 dbc.Row([
                     dbc.Col([
-                        dbc.CardImg(src="/assets/images/hat.png", className="clothes-image"),
+                        dbc.CardImg(src="/assets/images/head.png", className="clothes-image"),
                     ]),
                     dbc.Col([
                         html.H5("Brown Flatcap"),
@@ -103,18 +113,46 @@ def main():
 
 
     app.layout = html.Div([
-            navbar,
-            dbc.Row([
-                dbc.Col(card, width=4),
-                dbc.Col([
-                    dbc.Row([cardHat]),
-                    dbc.Row([cardShirt, cardJacket]),
-                    dbc.Row([cardPants]),
-                    dbc.Row([cardShoes]),
-                    # dbc.Row([cardHat, cardJacket, cardShirt, cardPants, cardShoes])
-                ], width=8),
-            ]),
-        ])
+        navbar,
+        dbc.Row([
+            dbc.Col(card, width=4),
+            dbc.Col([
+                dbc.Row([cardHat]),
+                dbc.Row([cardShirt, cardJacket]),
+                dbc.Row([cardPants]),
+                dbc.Row([cardShoes]),
+                # dbc.Row([cardHat, cardJacket, cardShirt, cardPants, cardShoes])
+            ], width=8),
+        ]),
+        dcc.Geolocation(id="geolocation"),
+        dcc.Store(id="weatherapi-data", storage_type="session", data="dict")
+    ])
+
+    @callback(
+        Output("weatherapi-data", "data"),
+        Input("geolocation", "position"),
+    )
+    def populate_data(pos):
+        if pos:
+            q = f"{pos['lat']},{pos['lon']}"
+            return api_instance.realtime_weather(q)
+        return no_update
+
+    @callback(
+        Output("location", "children"),
+        Output("temperature", "children"),
+        Input("temp-format", "value"),
+        Input("weatherapi-data", "data")
+    )
+    def display_data(format, store):
+        if store:
+            town = store['location']['name']
+            country = store['location']['country']
+            temp_string = "temp_f" if format else "temp_c"
+            temp = store['current'][temp_string]
+            return f"{town}, {country}", f"{temp}°"
+        return no_update, no_update
+
     return app.server
 
 if __name__ == "__main__":
